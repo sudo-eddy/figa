56223cb13ac4f38c2f1e3cc83f14b61a79fdd9dc
Eddy <sudo.coding@protonmail.com>
2025-08-27 00:24:39 -0500

    add missing type

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
+import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // Disable ESLint during builds
-  },
 };
 
 export default nextConfig;
